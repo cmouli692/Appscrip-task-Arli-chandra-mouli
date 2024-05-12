3f9a0fee75cacafa1cb6c9b93f900c8f7b83d7ba
Your Name <cmouli692@gmail.com>
2024-05-12 18:01:18 +0530

    added recommender drop down

--- src/components/RecommendedAndFilterComponent/index.js
+++ src/components/RecommendedAndFilterComponent/index.js
@@ -5,10 +5,17 @@ const RecommendedAndFilterComponent = () => (
         <h1 className="filter-text">Filter</h1>
         <hr className="hr-between-filter-and-recommended"/>
         <div className="recommended-text-arrow-left-container"> 
-            <h1 className="recommended-text">Recommended</h1>
-            <div className="arrow-left-img-container">
+            <select className="select-container">
+                <option className="recommended-text">Recommended</option>
+                <option className="recommended-text">NEWEST FIRST</option>
+                <option className="recommended-text">POPULAR</option>
+                <option className="recommended-text">PRICE : HIGH TO LOW</option>
+                <option className="recommended-text">PRICE : LOW TO HIGH</option>
+            </select>
+            {/* <h1 className="recommended-text">Recommended</h1> */}
+            {/* <div className="arrow-left-img-container">
                 <img src="../.././arrow-left.png" alt="arrow-left"/>
-            </div>   
+            </div>    */}
         </div>   
     </div>
 )
